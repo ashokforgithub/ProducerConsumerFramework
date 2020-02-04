c3689979fecbbc24181b69f104145f0b74058d28
ashokforgithub <ashok.track1@gmail.com>
2020-02-05 00:41:46 +0530

    Added Files for Event Store

---
+++ src/main/java/com/mycomp/prodcons/framework/filesystem/util/RandomNumberGenerator.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.mycomp.prodcons.framework.filesystem.util;
+
+import java.util.Random;
+
+/**
+ * @author Ashok
+ *
+ */
+public class RandomNumberGenerator {
+	
+	private static Random random = new Random();
+	
+	private static int seed= 1000;
+	
+	/**
+	 *  hiding constructor
+	 */
+	private RandomNumberGenerator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @param numberUpperBound
+	 * @return
+	 */
+	public static int randomInt(int numberUpperBound)
+	{
+		return random.nextInt(numberUpperBound);
+	}
+	
+	
+	/**
+	 * @return
+	 */
+	public static int randomInt()
+	{
+		return random.nextInt(seed);
+	}
+
+	/**
+	 * @param numberUpperBound
+	 * @return
+	 */
+	public static double randomDouble(int numberUpperBound)
+	{
+		return random.nextInt(numberUpperBound);
+	}
+	
+	
+	/**
+	 * @return
+	 */
+	public static double randomDouble()
+	{
+		return random.nextInt(seed);
+	}
+
+
+}
